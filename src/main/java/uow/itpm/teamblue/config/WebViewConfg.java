94a9a09e788daff94b64eb7e64772818a637e6a5
ndpdarshana <ndp.mymail@gmail.com>
2018-08-15 22:33:13 +1000

    static web page path tested

---
+++ src/main/java/uow/itpm/teamblue/config/WebViewConfg.java
@@ -0,0 +1,20 @@
+package uow.itpm.teamblue.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebViewConfg {
+
+    @Bean
+    public WebMvcConfigurer forwardToIndex(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addRedirectViewController("/", "/index.html");
+            }
+        };
+    };
+}
