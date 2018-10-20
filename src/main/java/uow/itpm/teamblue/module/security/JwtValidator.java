c22247aaabb2500996c69a4cc5b29db6b46436d2
ndpdarshana <ndp.mymail@gmail.com>
2018-10-21 01:40:24 +1100

    Copyright spelling mistake fix

--- src/main/java/uow/itpm/teamblue/module/security/JwtValidator.java
+++ src/main/java/uow/itpm/teamblue/module/security/JwtValidator.java
@@ -3,7 +3,7 @@
  *
  * This file is a part of class project in CSCI814 - Teamblue, UOW.
  *
- * This code can not be copied of reuse until CSCI814 2018 Spring session grading release date of 29 November 2018.
+ * This code can not be copied or reuse until CSCI814 2018 Spring session grading release date of 29 November 2018.
  * Written by Prabhath Darshana <pdnd723@uowmail.edu.au>
  */
 
